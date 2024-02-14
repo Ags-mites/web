c9b68d81e33392a5c0c9cb654dd83b978768de18
ags-mites <eamites1@espe.edu.ec>
2024-02-13 20:56:07 -0500

    primer commit

---
+++ Letter.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".valentines-day").click(function(){
+      $(".card-hide").removeClass("hide").addClass("show");
+      $(".home").removeClass("show").addClass("hide");
+    });
+  });
